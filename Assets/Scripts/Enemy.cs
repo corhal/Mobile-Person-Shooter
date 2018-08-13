82194f7517984a2e350f0c8690d7dac3d3ef0bcf
Andrey <corhal@yandex.ru>
2018-08-13 10:08:12 +0300

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public int HP;
+	ParticleSystem hitParticles;
+
+	void Awake () {
+		hitParticles = GetComponentInChildren<ParticleSystem> ();
+	}
+
+	public void TakeDamage (int amount, Vector3 hitPoint) {
+		hitParticles.transform.position = hitPoint;
+
+		// And play the particles.
+		hitParticles.Play();
+
+		HP -= amount;
+		if (HP <= 0) {
+			Destroy (gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+		
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButton ("Fire1")) {
+			Ray shootRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			transform.LookAt (shootRay.GetPoint(10.0f));
+		}
+	}
+}
